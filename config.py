583d17a8639d763d3d5d0fd19c11f9a7cbe89a5e
Edward69520 <86703049+Edward69520@users.noreply.github.com>
2022-01-04 00:31:37 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# Your Bot token in the quotes taken from @BotFather
+BOT_TOKEN = "5094843152:AAE_FMuH5lNBG3cA7ar8FEbyKxJFEla0iRc"
+
+# Project Name of your Project
+PROJECT_NAME = "zurkbot"
+
+# If you forked the source code, grab your repo's link and paste it below!
+GIT_REPO_URL = "https://github.com/Edward69520/zurkbot"
